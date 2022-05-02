4e0b58383a8fe3dd25655ace1b5f9bd580884a9d
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 15:37:35 -0700

    added index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,33 @@
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_products",
+});
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tags",
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
