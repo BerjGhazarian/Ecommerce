e1a39b4dc4ecaeb5c97e68e18a6246923e5f79aa
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 15:22:16 -0700

    get and post route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,3 +13,29 @@ router.get('/', async (req, res) => {
   }
 
 });
+router.get('/:id', async (req, res) => {
+    // find one category by its `id` value
+    try {
+      const categoryData = await Category.findByPk(req.params.id, {
+        include: [{ model: Product}],
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: "No category found with that id" });
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.post('/', async (req, res) => {
+    // create a new category
+    try {
+      const categoryData = await Category.create(req.body);
+      res.status(200).json(categoryData)
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  });
